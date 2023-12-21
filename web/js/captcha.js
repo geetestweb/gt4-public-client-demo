b2d163a321142e3e5d3827f351f55bbb6b8c74c2
yangyawen <786878243@qq.com>
2023-12-21 16:35:28 +0800

    flutter-web demo init

--- web/js/captcha.js
+++ web/js/captcha.js
@@ -1,9 +1,9 @@
 /*
  * @Author: yangyawen 786878243@qq.com
  * @Date: 2023-10-11 10:58:32
- * @LastEditors: yangyawen 786878243@qq.com
- * @LastEditTime: 2023-11-08 15:49:40
- * @FilePath: /flutter/my_app/web/js/captcha.js
+ * @LastEditors: yawen Yang
+ * @LastEditTime: 2023-12-21 16:35:16
+ * @FilePath: /gt4-public-client-demo/web/js/captcha.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var showCaptcha = function(){}
@@ -12,7 +12,7 @@ var destroyCaptcha = function(){}
 
 function initCaptcha(){
   initGeetest4({
-    captchaId: "21dd8f9031049308efb5d1f16b90004e",
+    captchaId: "xxxx",
     product: 'bind',
     language: 'eng'
 },function (captcha) {
